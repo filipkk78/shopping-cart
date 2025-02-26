646c921182af532574bda850394994f1c929b71d
filipkk78 <filipkk78@gmail.com>
2025-02-26 17:56:08 +0100

    Improve error page

--- src/components/ErrorPage/ErrorPage.jsx
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -1,14 +1,18 @@
 import { Link } from "react-router-dom";
 import styles from "./ErrorPage.module.css";
+import Header from "../Header/Header";
 
 const ErrorPage = () => {
   return (
-    <div>
-      <h1>Oh no, this route doesn't exist!</h1>
-      <Link to="/">
-        You can go back to the home page by clicking here, though!
-      </Link>
-    </div>
+    <>
+      <Header></Header>
+      <main className={styles.main}>
+        <h1>Oh no, this route doesn't exist!</h1>
+        <Link to="/">
+          You can go back to the home page by clicking here, though!
+        </Link>
+      </main>
+    </>
   );
 };
 
